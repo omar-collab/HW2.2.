bfd25e5f9e0f2fa8befc058f14dde396e5eb978f
omar-collab <cerambite132@gmail.com>
2020-11-01 15:47:58 +0600

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    private String animal;
+
+    public Animal(String animal) {
+        this.animal = animal;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+
+
+}
---
+++ src/Dog.java
@@ -0,0 +1,16 @@
+public class Dog extends Animal implements Printable {
+    private int dog;
+
+
+    public Dog(String animal) {
+        super(animal);
+        this.dog = dog;
+    }
+
+
+    @Override
+    public void print() {
+        System.out.println("\uD83D\uDC36");
+        System.out.println("Гаф");
+    }
+}
